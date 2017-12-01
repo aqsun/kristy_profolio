c24948bdf321ab704551434ba33940e47fa9bc8c
Unknown <aqsun@sfu.ca>
2017-11-30 16:17:20 -0800

    modified hover problem, button probem

--- js/normalize.js
+++ js/normalize.js
@@ -1,3 +1,9 @@
+$( document ).ready(function() {
+  if(location.href.indexOf("#About") != -1) {
+    showIntroduction();
+  }
+});
+
 function showIntroduction() {
     $('#introduction').removeClass('notShow');
     $('#introduction-title').addClass('notShow');
